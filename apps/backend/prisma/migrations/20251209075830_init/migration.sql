79e92c46bb3975cfb93a92063aca0cf7bde42fde
Raja6284 <rajaraja360parth@gmail.com>
2025-12-09 13:33:51 +0530

    feat: project setup

---
+++ apps/backend/prisma/migrations/20251209075830_init/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "bookings" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "messages" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "notifications" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "payments" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "reviews" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "id" DROP DEFAULT;
